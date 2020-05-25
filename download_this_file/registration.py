e12aa82f3dc339c103530ee5498b9da6124c5b9c
puaqieshang <qieshang@gmail.com>
2020-05-25 17:06:01 +1000

    rishav sucks

--- download_this_file/registration.py
+++ download_this_file/registration.py
@@ -9,52 +9,54 @@ Output: A figure with 3 sub-plots   - STL Model (Ground Truth)
 
 Note: This program is only valid for phantom 3 scan 2
 """
+
+# Modules to import
 import numpy as np
 import math
-
-import openmesh
 import pandas as pd
-from stl import mesh
-from mpl_toolkits import mplot3d
-import matplotlib.pyplot
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from stl.mesh import Mesh
-import vtkplotlib as vpl
 import open3d
-import scipy
+import scipy.spatial
 import scipy.io
-import openmesh as om
 import copy
-from pyntcloud import PyntCloud
+import trimesh
+import matplotlib.pyplot as plt
 
 
+# ICP registration
+def draw_registration_result_original_color(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    source_temp.transform(transformation)
+    source_temp.paint_uniform_color([0.5, 0.5, 0.5])
+    open3d.visualization.draw_geometries([source_temp, target])
 
+def registration(raw):
 
-def registration(test):
-    correct = np.array(test)
-    numCols = len(test[2])
+    correct = np.array(raw)
+
+    # To shift up the points, 11 is a threshold
+    numCols = len(raw[2])
     to_shift_up = [0] * numCols
     for col in range(numCols):
-        if test[2][col] < 11:
-            to_shift_up[col] = 1
+        if raw[2][col] < 11:
+            to_shift_up[col] = 30
         else:
             to_shift_up[col] = 0
 
-    # print(to_shift_up)
-    test[0] = test[0] + np.multiply(30, to_shift_up)
+    raw[0] = raw[0] + to_shift_up
 
+    # Axes Transformation, values are selected through trial and error as per MATLAB code
     x_shift = -43.5;
     y_shift = 95;
     z_shift = 148;
 
-    correct[0] = test[1] * 90.0 / 569.0 + x_shift
-    correct[1] = (test[0] * (-88) / 569) + y_shift
-    correct[2] = test[2] * (-5.01) + z_shift
+    correct[0] = raw[1] * 90.0 / 569.0 + x_shift
+    correct[1] = (raw[0] * (-88) / 569) + y_shift
+    correct[2] = raw[2] * (-5.01) + z_shift
 
     np.set_printoptions(precision=3)  # Prints array in 3 decimal places
 
-    # THIS KIND OF ROTATOIN IS INCORRECT(ISH)
+    # Rotation
     skew_value = 0.08
     skew_y = np.array([[1, 0, skew_value], [0, 1, 0], [0, 0, 1]])
 
@@ -70,14 +72,12 @@ def registration(test):
     correct = np.dot(Ry, correct)
 
     # Get STL model
-
     path = "/Users/puaqieshang/Desktop/Taste of Research/everything/models/Segmentation_bone.stl"
-    meshy = Mesh.from_file(path)
-    points = np.around(np.unique(meshy.vectors.reshape([meshy.vectors.size // 3, 3]), axis=0), 2)  # p2.location
+    stl_mesh = Mesh.from_file(path)
+    points = np.around(np.unique(stl_mesh.vectors.reshape([stl_mesh.vectors.size // 3, 3]), axis=0), 2)  # p2.location
 
-    # vpl.mesh_plot(meshy)
-    # vpl.show()
-
+    # Displays the model, eliminate outlier points that are too far
+    # Range values for each axis as selected per MATLAB code
     minz = -200
     maxz = 400
 
@@ -86,160 +86,118 @@ def registration(test):
 
     minx = -100
     maxx = 500
+
+    # p represents point cloud
     px = np.bitwise_and(points[0, :] > minx, points[0, :] < maxx)
     py = np.bitwise_and(points[1, :] > miny, points[1, :] < maxy)
     pz = np.bitwise_and(points[2, :] > minz, points[2, :] < maxz)
     p = np.bitwise_and(px, py, pz)
 
+    # c is the colour map
     cx = np.bitwise_and(correct[0, :] > minx, correct[0, :] < maxx)
     cy = np.bitwise_and(correct[1, :] > miny, correct[1, :] < maxy)
     cz = np.bitwise_and(correct[2, :] > minz, correct[2, :] < maxz)
     c = np.bitwise_and(cx, cy, cz)
 
-    fixed_points = np.transpose(correct)  # p1.location
+    segmented_points = np.transpose(correct)
 
-    # figure = pyplot.figure()
-    # axes = mplot3d.Axes3D(figure)
-    #
-    # model = mesh.Mesh.from_file('/Users/puaqieshang/Desktop/Taste of Research/everything/models/Segmentation_bone.stl')
-    # axes.add_collection3d(mplot3d.art3d.Poly3DCollection(model.vectors))
-    # scale = model.points.flatten(-1)
-    # axes.auto_scale_xyz(scale, scale, scale)
-
-    # pyplot.show()
-    # points = np.around(np.unique(mesh.vectors.reshape([mesh.vectors.size / 3, 3]), axis=0), 2)
-
-    num_rows = np.ma.size(fixed_points, 0)
+    num_rows = np.ma.size(segmented_points, 0)
     points = np.transpose(points)
-    ble = np.ones((num_rows, 1))
 
+    # Array to store the error for colour map
+    error = np.ones((num_rows, 1))
+
+    # For loop determines the colour map or the error when mapping segmented points to stl
     for i in range(num_rows):  # 39372
-        point = fixed_points[i, :]  # extracting each row from p1
+        point = segmented_points[i, :]  # extracting each row from p1
         point_p2 = points[:, i]
+
+        # euclidean distance between the segmented points and the stl model
         dist = scipy.spatial.distance.euclidean(point_p2, point)
         dist = dist / 100
 
         if (dist > 10):
-            ble[i] = 0;
+            error[i] = 0; # if the error is too big then it won't display on the colour map because
+                          # it is a point of less interest
         elif np.mean(points[p, 1]) < point[1]:
-            ble[i] = np.mean(dist)
+            error[i] = np.mean(dist)
         else:
-            ble[i] = np.mean(dist)
+            error[i] = np.mean(dist)
 
+    # ignore negative errors
     for i in range(num_rows):
-        if (ble[i] < 0):
-            ble[i] = 0
+        if (error[i] < 0):
+            error[i] = 0
 
-
-    highest = max(ble)
-    lowest = min(ble)
+    # Outputs maximum and minimum error
+    highest = max(error)
+    lowest = min(error)
     print(f"The maximum error is {highest}mm")
     print(f"The minimum error is {lowest}mm")
 
-    temp = ble;
+    # Use a temp variable to store the error array of each point
+    temp = error;
 
-
-
-    ble = ble * 255 / highest
-    # print(ble)
+    # Convert error array into rgb and store in colours array
+    error = error * 255 / highest
     zeroes = np.zeros((39372, 2))
+    colours = np.append(zeroes, error, axis=1)
 
-    colours = np.append(zeroes, ble, axis=1)
-    # print(ble)
+    # Displays colour map depending on error, the threshold are selected using trial and error
     for i in range(len(temp)):
         if temp[i] > 1.7:
-            colours[i] = [0, 255, 0] # green colour means less accurate
+            colours[i] = [0, 255, 0] # green colour means more error
             continue
 
         if temp[i] < 0.5:
-            colours[i] = [0, 0, 255] # blue colour means more accurate
+            colours[i] = [0, 0, 255] # blue colour means less error
             continue
 
 
+    # Display the STL Point Cloud and Segmented Points
     pcd_image = open3d.geometry.PointCloud()
-    pcd_image.points = open3d.utility.Vector3dVector(fixed_points)
+    pcd_image.points = open3d.utility.Vector3dVector(segmented_points)
     pcd_image.colors = open3d.utility.Vector3dVector(colours)
-    print(colours)
-    print(colours.shape)
-    open3d.io.write_point_cloud("puaiiiii.ply", pcd_image)
-    pcd_image = open3d.io.read_point_cloud("puaiiiii.ply")  # Read the point cloud
-    # open3d.visualization.draw_geometries([pcd_image])
 
+    # Writing segmented points cloud to an open3d image
+    open3d.io.write_point_cloud("point_cloud_segmented.ply", pcd_image)
+    # Read the point cloud
+    pcd_image = open3d.io.read_point_cloud("point_cloud_segmented.ply")
 
-    import trimesh
-
+    # Load original STL file
     mesh1 = trimesh.load('/Users/puaqieshang/Desktop/Taste of Research/everything/models/Segmentation_bone.stl')
+    # Convert stl file to ply
     mesh2 = mesh1.copy()
-    mesh2.export('stuff_stl.ply')
+    mesh2.export('convertedSTL.ply')
 
-    def draw_registration_result_original_color(source, target, transformation):
-        source_temp = copy.deepcopy(source)
-        source_temp.transform(transformation)
-        source_temp.paint_uniform_color([0.5, 0.5, 0.5])
-        open3d.visualization.draw_geometries([source_temp, target])
-    
-    
-    source = open3d.io.read_point_cloud("stuff_stl.ply")
-
-    target = open3d.io.read_point_cloud("puaiiiii.ply")
+    # Set parameters for icp registration
+    source = open3d.io.read_point_cloud("convertedSTL.ply")
+    target = open3d.io.read_point_cloud("point_cloud_segmented.ply")
     threshold = 0.005
+    # Transformation matrix
     trans_init = np.asarray([[1, 0, 0, 0],[0, 1, 0, 0],[0,0, 1, 0], [0, 0, 0, 1]])
-    # draw_registration_result(source, target, trans_init)
 
-    # target.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-    # source.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-
-
-
+    # Evaluate registration performance
     evaluation = open3d.registration.evaluate_registration(source, target,threshold, trans_init)
-    reg_p2p = open3d.registration.registration_icp(source, target, threshold, trans_init,open3d.registration.TransformationEstimationPointToPoint())
+
+    # Obtain a registered model
+    reg_p2p = open3d.registration.registration_icp(source, target, threshold,
+                                               trans_init,open3d.registration.TransformationEstimationPointToPoint())
+
+    # Plot registered model
     draw_registration_result_original_color(source, target, reg_p2p.transformation)
-    # reg_p2l = open3d.registration.registration_icp(source, target, threshold, trans_init, open3d.registration.TransformationEstimationPointToPlane())
-    # draw_registration_result_original_color(source, target, reg_p2l.transformation)
 
-
-
-    # print(result_icp)
-    
-    # openmesh.write_mesh(mesh1, "mesh_stl.ply")
-    # pupu = mesh1
-    # print(pupu)
-    # import pymesh
-    # mesh2 = pymesh.load_mesh("/Users/puaqieshang/Desktop/Taste of Research/everything/models/Segmentation_bone.stl")
-    # pymesh.save_mesh("meshTest.ply", mesh2, ascii = True)
-    # print(mesh2)
-
-    # human_face = PyntCloud.from_file("puaiiiii.ply")
-    #########
-    # cm = plt.cm.get_cmap('RdGy')
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # xs = fixed_points[:, 0]
-    # ys = fixed_points[:, 1]
-    # zs = fixed_points[:, 2]
-    #
-    #
-    # ax.set_xlabel('X Label')
-    # ax.set_ylabel('Y Label')
-    # ax.set_zlabel('Z Label')
-    #
-    # inn = ax.scatter(xs, ys, zs, s=10, c=(colours[:, 0]) / 255 , depthshade=True, cmap = cm)
-    # plt.colorbar(inn)
-    # plt.show()
-    import matplotlib.pyplot as plt
-    # print()
+    # Plot the error histogram
     plt.hist(temp, bins=50)
     plt.gca().set(title='Error/Distance', ylabel='Frequency');
     plt.show()
 
+# Main file
+def main():
 
-    # https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/multi_image.html#sphx-glr-gallery-images-contours-and-fields-multi-image-py
-    # CHECK THIS WEBSITE FOR MULTIPLE SUBPLOTS
+    # Retrieve segmented_points data by running registration.m on MATLAB
+    segmented_points_path = "/Users/puaqieshang/Desktop/raw_segmented_points.csv"
+    df = pd.read_csv(segmented_points_path, header=None)
+    registration(np.array(df))
 
-
-# read from csv
-file_location = "/Users/puaqieshang/Desktop/Taste of Research/everything/inital_experiments/pua.csv"  # saved from points.mat from MATLAB
-# file_location = "/Users/puaqieshang/Desktop/Taste of Research/original code [DO NOT EDIT]/inital_experiments/pua_phantom.csv"
-df = pd.read_csv(file_location, header=None)
-df = np.array(df)
-registration(np.array(df))  # Make it a np array - easier
+main()
