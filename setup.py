c9c8dd8f14b9c2eb296bf367a8e606890000dbf3
puaqieshang <qieshang@gmail.com>
2020-06-01 18:09:35 +1000

    added setup and rers

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ultrasound_rego',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/puaqieshang/ultrasound-image-segmentation',
+    license='University of New South Wales (UNSW), Australia',
+    author='Qie Shang Pua, Rishav Raj',
+    author_email='qieshang@gmail.com',
+    description='A package for Ultrasound Image Segmentation and Registration using Python'
+)
